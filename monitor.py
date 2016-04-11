a81778c7e70bd3962df396d4254bff8c49a26c25
Thomas Tsai <thomas@nchc.org.tw>
2016-04-11 16:48:38 +0800

    add sample code

---
+++ monitor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import time
+import datetime
+import os
+import httplib, urllib
+import g3
+
+air=g3.g3sensor()
+while True:
+    try:
+        pmdata=air.read("/dev/ttyAMA0")
+    except:
+        pmdata=[0,0,0,0,0,0]
+	continue
+
+    # thingspeak
+    params = urllib.urlencode({'field1': pmdata[3], 'field2': pmdata[4], 'field3': pmdata[5], 'key':'YOUR WRITE KEY'})
+    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    try:
+        tconn = httplib.HTTPConnection("api.thingspeak.com:80")
+        tconn.request("POST", "/update", params, headers)
+        response = tconn.getresponse()
+        data = response.read()
+        tconn.close()
+    except:
+        continue
+    time.sleep(600)
+
+
